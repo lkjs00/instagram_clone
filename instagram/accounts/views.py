fa03eb0df56cda72f1ca2130cb63726f9f647e37
이준현 <junhyun1993@daum.net>
2020-05-15 12:40:53 +0000

    first commit

---
+++ instagram/accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect()
+    else:
+        form = UserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
---
+++ instagram/articles/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
