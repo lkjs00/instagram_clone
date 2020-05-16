5cc6e74d4786a26f17850642879463668c088921
이준현 <junhyun1993@daum.net>
2020-05-16 04:59:23 +0000

    order_by(-pk)

--- instagram/articles/admin.py
+++ instagram/articles/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Article, Comment
 
 # Register your models here.
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'created_at', 'updated_at', 'user']
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['id', 'content', 'article', 'user']
+
 admin.site.register(Article, ArticleAdmin)
 admin.site.regitser(Comment)
