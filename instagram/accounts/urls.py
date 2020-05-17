1ffd339647530e028c76933ca04fc890ef65cf17
이준현 <junhyun1993@daum.net>
2020-05-17 01:44:15 +0000

    Accounts detail page

--- instagram/accounts/urls.py
+++ instagram/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('<int:user_pk>/', views.detail, name='detail'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
--- instagram/accounts/views.py
+++ instagram/accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -16,6 +17,14 @@ def signup(request):
     }
     return render(request, 'accounts/signup.html', context)
 
+def detail(request, user_pk):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=user_pk)
+    context = {
+        'user': user,
+    }
+    return render(request, 'accounts/detail.html', context)
+    
 def login(request):
     pass
 
